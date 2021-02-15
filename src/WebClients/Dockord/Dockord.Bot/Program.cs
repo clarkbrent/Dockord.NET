cd88e3e4b30e1cfa3edd542baf2dee4ff0d15cbf
Clark Brent <brentcostanzo@gmail.com>
2021-02-14 22:54:55 -0800

    Add project files

---
+++ src/WebClients/Dockord/Dockord.Bot/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Dockord.Bot
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting app...");
+            Console.ReadLine();
+        }
+    }
+}
